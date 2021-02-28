9c63afd475324b818ebf2215477da01b929e3560
Igorg599 <igorg599@gmail.com>
2021-02-28 22:03:33 +0500

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Scena from './Scene';
+
+function App() {
+  return <Scena/>
+}
+
+export default App;
---
+++ src/Scene.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { Engine, Scene } from 'react-babylonjs'
+// import { Vector3 } from '@babylonjs/core';
+
+const Scena = () => (
+  <div className="row">
+    1
+  </div>
+)
+
+export default Scena
