2e814fe38224eb9733c90079a43ca9d900a6c1d0
Igorg599 <igorg599@gmail.com>
2021-03-04 21:35:13 +0500

    наложение_цветов

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -21,10 +21,10 @@ const View: () => JSX.Element = () => {
                     alpha={-Math.PI / 2} beta={(0.5 + (Math.PI / 4))}
                     radius={38} minZ={0.001} wheelPrecision={50}/>
           <Skybox rootUrl={SkyboxScenes[Math.abs(0) % SkyboxScenes.length].texture} />
-          <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />   
+          <hemisphericLight name="light" intensity={0.7} direction={Vector3.Up()} />   
           {items && items.map((item: any, index: any) => (
-            <box key={index} name='box' id='11' width={0.1} depth={0.1} height={Vector3.Distance(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y), new Vector3(item[0].point.x, item[0].point.z, item[0].point.y))}  rotationQuaternion={Quaternion.RotationAxis(Vector3.Cross(new Vector3(item[0].point.x, item[0].point.z, item[0].point.y).subtract(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y)).normalize(), new Vector3(0, 1, 0)).normalize(), -Math.acos(Vector3.Dot(new Vector3(item[0].point.x, item[0].point.z, item[0].point.y).subtract(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y)).normalize(), new Vector3(0, 1, 0))))} position={new Vector3((item[0].point.x + item[1].point.x)/2, (item[0].point.z + item[1].point.z)/2, (item[0].point.y + item[1].point.y)/2)}>
-              <standardMaterial name='groundMat' specularColor={Color3.Green()} diffuseColor={Color3.Yellow()}/>
+            <box  key={index} name='box' id='11' width={0.1} depth={0.1} height={Vector3.Distance(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y), new Vector3(item[0].point.x, item[0].point.z, item[0].point.y))}  rotationQuaternion={Quaternion.RotationAxis(Vector3.Cross(new Vector3(item[0].point.x, item[0].point.z, item[0].point.y).subtract(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y)).normalize(), new Vector3(0, 1, 0)).normalize(), -Math.acos(Vector3.Dot(new Vector3(item[0].point.x, item[0].point.z, item[0].point.y).subtract(new Vector3(item[1].point.x, item[1].point.z, item[1].point.y)).normalize(), new Vector3(0, 1, 0))))} position={new Vector3((item[0].point.x + item[1].point.x)/2, (item[0].point.z + item[1].point.z)/2, (item[0].point.y + item[1].point.y)/2)}>
+              <standardMaterial   name='groundMat' diffuseColor={new Color3(Math.random(), Math.random(), Math.random()/2)}/>
             </box>
           ))}
         </Scene>
